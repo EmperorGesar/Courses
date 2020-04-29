2a14b3a6b0edd19e121e664efb778460e9b5417d
EmperorGesar <50392401+EmperorGesar@users.noreply.github.com>
2020-04-29 17:45:43 -0400

    Create member2.pl

---
+++ cs3342/asn4/member2.pl
@@ -0,0 +1,10 @@
+member2(X, L) :- member3(X, L, []).
+
+member3(X, [X|_], R) :- not(member1(X, R)).
+member3(X, [H|T], R) :- member3(X, T, [H|R]).
+
+member1(X, [X|_]) :- !.
+member1(X, [_|T]) :- member1(X, T).
+
+not(X) :- X, !, fail.
+not(_).

968607612dfeb19ec715dac14a6800846a8ce8fb
EmperorGesar <50392401+EmperorGesar@users.noreply.github.com>
2020-04-29 17:10:17 -0400

    Create quicksort.rkt

---
+++ cs3342/asn3/quicksort.rkt
@@ -0,0 +1,26 @@
+#lang racket
+;Quick sort algorithm
+
+(define quicksort
+  (lambda (L)
+    (if (null? L)
+        '()
+        (letrec ((pre (lambda (rest)
+                        (if (null? rest)
+                            '()
+                            (if (> (car L) (car rest))
+                                (cons (car rest) (pre (cdr rest)))
+                                (pre (cdr rest))))))
+                 (post (lambda (rest)
+                         (if (null? rest)
+                             '()
+                             (if (< (car L) (car rest))
+                                 (cons (car rest) (post (cdr rest)))
+                                 (post (cdr rest))))))
+                 (combine (lambda (head tail)
+                            (if (null? head)
+                                tail
+                                (cons (car head) (combine (cdr head) tail))))))
+          (combine (combine (quicksort (pre L)) (list (car L))) (quicksort (post L)))))))
+
+(quicksort '(5 3 7 2 1 6 4))
